0cc6d43bf87a5f8921bf285515098e0ab0f6e89b
dlocc <dlocc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-08 02:25:24 +0000

    Release of version 1.2.5.3
    git-svn-id: https://plugins.svn.wordpress.org/subscribe2-widget-pro/tags/1.2.5.3@784267 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/templates/widget-output.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ *  Widget Frontend Output
+ *
+ * @description: The frontend widget form output
+ * @since: 1.0
+ * @created: 4/28/13
+ */
+
+if (isset($before_widget)) echo $before_widget;
+
+//Hide Widget Title if option enabled
+if (!$hide_widget_title == 'hide') {
+    if (isset($before_title)) echo $before_title;
+    if (isset($title)) echo $title;
+    if (isset($after_title)) echo $after_title;
+}
+
+//Display Success Message for signed up users (non-AJAX submissions)
+if (isset($_POST["subscribe"])) {
+
+    echo "<div class='s2w-alert s2w-success'>" . (!empty($success_message) ? $success_message : __("Thank you for subscribing, please check your email to confirm your subscription.", 's2w')) . "</div>";
+
+} elseif (isset($_POST["unsubscribe"])) {
+
+    echo "<div class='s2w-alert s2w-success'>" . (!empty($unsubscribe_message) ? $unsubscribe_message : __("Subscription removal request received. Please check your email to confirm your request.", 's2w')) . "</div>";
+
+} else {
+//Show Widget
+
+    //Determine where this form will post data to
+    if (!empty($postto) && $postto !== 'home') {
+        $postPermalink = get_permalink($postto);
+    } elseif ($postto == 'home') {
+        $postPermalink = get_bloginfo('wpurl');
+    }
+
+
+    ?>
+
+    <div class="s2w-widget-pro<?php
+    //Output Custom DIV class
+    if (!empty($div)) {
+        echo ' ' . $div;
+    }; ?>" data-validation-error="<?php echo $validation_message; ?>">
+
+        <?php
+        if (!empty($widgetprecontent)) {
+            echo wpautop($widgetprecontent);
+        }
+        ?>
+
+        <form action="<?php echo $postPermalink; ?>" method="post" class="s2w-form">
+
+            <input type="hidden" value="<?php echo s2w_get_IP(); ?>" name="ip">
+
+            <label for="s2email"><?php _e('Your email:', 's2w'); ?></label><br><input type="text" onblur="if (this.value == '') {this.value = 'Enter email address...';}" onfocus="if (this.value == 'Enter email address...') {this.value = '';}" size="20" value="Enter email address..." id="s2email" class="s2w-email" name="email">
+
+            <?php if ($hide_subscribe_button != 'none') { ?>
+                <input type="submit" value="Subscribe" name="subscribe">
+            <?php } ?>
+
+            <?php
+            /**
+             * Show Unsubscribe button if checked
+             */
+            if ($hide_unsubscribe_button == 'show') {
+                ?>
+                <input type="submit" value="Unsubscribe" name="unsubscribe">
+            <?php } ?>
+
+            <?php if ($disable_antispam !== 'no') { ?>
+                <span style="display:none !important">
+                              <label for="name">Leave Blank:</label>
+                              <input type="text" class="s2w-name" name="name"/>
+                              <label for="uri">Don't Change:</label>
+                              <input type="text" class="s2w-uri" name="uri" value="http://"/>
+                          </span>
+            <?php } ?>
+
+        </form>
+
+        <?php
+        if (!empty($widgetpostcontent)) {
+            echo wpautop($widgetpostcontent);
+        }
+        ?>
+
+    </div>
+
+<?php } //end check for $_POST submission  ?>
+<?php if (isset($after_widget)) echo $after_widget; ?>
